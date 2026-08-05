fc449b0cf8c55fdb95f1b6fe162b420fe8f8505c
Daniel Zhou <danielzhou.nc@gmail.com>
2026-08-05 16:58:13 -0400

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        mySet = set()
+        for n in nums:
+            if n in mySet:
+                return True
+            else:
+                mySet.add(n)
+
+        return False
+         
